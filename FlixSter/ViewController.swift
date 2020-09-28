81b8fb97b74410da8dbd5dbc508577428e33ef0c
Claudine250 <60280035+Claudine250@users.noreply.github.com>
2020-09-28 08:21:20 -0500

    Initial Commit

---
+++ FlixSter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixSter
+//
+//  Created by claudine  on 9/28/20.
+//  Copyright © 2020 claudineu250@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
